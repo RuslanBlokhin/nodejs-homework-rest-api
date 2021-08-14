ec1c2044435ceeed98692c8cb8f6405a22be77e9
Russel <blokhin.rus@gmail.com>
2021-08-14 16:16:39 +0300

    first part

--- app.js
+++ app.js
@@ -1,25 +1,70 @@
-const express = require('express')
-const logger = require('morgan')
-const cors = require('cors')
+const express = require("express");
+const logger = require("morgan");
+const cors = require("cors");
+const mongoose = require("mongoose");
+require("dotenv").config();
 
-const contactsRouter = require('./routes/api/contacts')
+const { DB_HOST } = process.env;
 
-const app = express()
+const { Schema, model } = mongoose;
 
-const formatsLogger = app.get('env') === 'development' ? 'dev' : 'short'
+const contactSchema = Schema({
+  name: {
+    type: String,
+    required: [true, "Set name for contact"],
+  },
+  email: {
+    type: String,
+  },
+  phone: {
+    type: String,
+  },
+  favorite: {
+    type: Boolean,
+    default: false,
+  },
+});
 
-app.use(logger(formatsLogger))
-app.use(cors())
-app.use(express.json())
+const Contact = model("contact", contactSchema);
 
-app.use('/api/contacts', contactsRouter)
+mongoose
+  .connect(DB_HOST, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useCreateIndex: true,
+  })
+  .then(async () => {
+    const newContact = {
+      name: "Scott",
+      lastName: "Bekker",
+      email: "bekker@mail.ru",
+    };
+    try {
+      const result = await Contact.create(newContact);
+      console.log("Database connection successful");
+    } catch (error) {
+      console.log(error);
+    }
+  });
+
+const contactsRouter = require("./routes/api/contacts");
+
+const app = express();
+
+const formatsLogger = app.get("env") === "development" ? "dev" : "short";
+
+app.use(logger(formatsLogger));
+app.use(cors());
+app.use(express.json());
+
+app.use("/api/contacts", contactsRouter);
 
 app.use((req, res) => {
-  res.status(404).json({ message: 'Not found' })
-})
+  res.status(404).json({ message: "Not found" });
+});
 
 app.use((err, req, res, next) => {
-  res.status(500).json({ message: err.message })
-})
+  res.status(500).json({ message: err.message });
+});
 
-module.exports = app
+module.exports = app;
